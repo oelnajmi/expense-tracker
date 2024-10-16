0babfdbf86e3891e285141a4e5b5dc9a806685e4
oelnajmi <oelnajmi@gmail.com>
2024-10-16 13:20:11 -0400

    update sign in button to dialog

--- components/SignIn.tsx
+++ components/SignIn.tsx
@@ -1,7 +1,38 @@
-import { auth, signIn } from "@/auth";
+import { signIn } from "@/auth";
 import { Button } from "./ui/button";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "./ui/dialog";
 
 export default async function SignIn() {
+  return (
+    <Dialog>
+      <DialogTrigger asChild>
+        <Button className="bg-black text-white">Log In</Button>
+      </DialogTrigger>
+      <DialogContent className="bg-white sm:max-w-[425px]">
+        <DialogHeader>
+          <DialogTitle className="text-center text-2xl font-bold">
+            Hello!
+          </DialogTitle>
+          <DialogDescription className="text-center">
+            Use your gmail to continue with ExpenseEase.
+          </DialogDescription>
+        </DialogHeader>
+        <div className="flex flex-col items-center mt-4">
+          <SignInButton />
+        </div>
+      </DialogContent>
+    </Dialog>
+  );
+}
+
+function SignInButton() {
   return (
     <form
       action={async () => {
@@ -9,7 +40,27 @@ export default async function SignIn() {
         await signIn("google");
       }}
     >
-      <Button className="bg-black text-white">Log In</Button>
+      <Button
+        variant="outline"
+        className="w-full justify-start text-left font-normal"
+      >
+        <svg
+          className="mr-2 h-4 w-4"
+          aria-hidden="true"
+          focusable="false"
+          data-prefix="fab"
+          data-icon="google"
+          role="img"
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 488 512"
+        >
+          <path
+            fill="currentColor"
+            d="M488 261.8C488 403.3 391.1 504 248 504 110.8 504 0 393.2 0 256S110.8 8 248 8c66.8 0 123 24.5 166.3 64.9l-67.5 64.9C258.5 52.6 94.3 116.6 94.3 256c0 86.5 69.1 156.6 153.7 156.6 98.2 0 135-70.4 140.8-106.9H248v-85.3h236.1c2.3 12.7 3.9 24.9 3.9 41.4z"
+          ></path>
+        </svg>
+        Continue with Google
+      </Button>
     </form>
   );
 }
